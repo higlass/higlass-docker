3b26649623afc7af656fea456ae4b0403a3d5fa6
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-02-07 11:34:39 -0500

    seem to be specifying root volume successfully

--- aws/new-server.sh
+++ aws/new-server.sh
@@ -13,19 +13,23 @@ GROUP_ID=`aws ec2 describe-security-groups \
           --query 'SecurityGroups[0].GroupId' \
           --output text`
 
-echo "GROUP_ID: $GROUP_ID"
+echo "GROUP_ID=$GROUP_ID"
 
+MAPPINGS='{ "DeviceName": "/dev/sda1", "Ebs": { "VolumeSize": 100 } }'
+
+# TODO: change to micro->medium
 INSTANCE_ID=`aws ec2 run-instances \
           --image-id ami-e13739f6 \
           --security-group-ids $GROUP_ID \
           --count 1 \
-          --instance-type t2.medium \
+          --instance-type t2.micro \
           --key-name $KEY_NAME \
           --query 'Instances[0].InstanceId' \
           --placement AvailabilityZone=us-east-1c \
+          --block-device-mappings "$MAPPINGS" \
           --output text`
 
-echo "INSTANCE_ID: $INSTANCE_ID"
+echo "INSTANCE_ID=$INSTANCE_ID"
 
 aws ec2 create-tags \
     --resources ${INSTANCE_ID} \
@@ -39,23 +43,24 @@ status() {
         --output text
 }
 
-set +e
 TRY=0;
 until [[ `status $INSTANCE_ID` == 'running' ]] || [[ $TRY -gt 20 ]]; do
     echo "try $TRY"
     (( TRY++ ))
     sleep 1
 done
-set -e
 
-# and then:
 IP=`aws ec2 describe-instances \
      --instance-ids $INSTANCE_ID \
      --query 'Reservations[0].Instances[0].PublicIpAddress' \
      --output text`
 
-echo "IP: $IP"
+echo "IP=$IP"
 
-# create mount point
-ssh -i ~/$KEY_NAME.pem ubuntu@$IP 'sudo mkdir /data'
+# The first SSH takes a while: I think it's that the machine is "running",
+# but it still takes a while for services to start up.
+
+ssh -i ~/$KEY_NAME.pem ubuntu@$IP \
+    -o StrictHostKeyChecking=no \
+    'sudo mkdir /data'
 
