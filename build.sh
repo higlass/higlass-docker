addc104e9c1a0288bc59a3d3f2b4a36a9f54e73f
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-12-16 00:17:29 -0500

    Bumped client version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 STAMP='default'
 SERVER_VERSION='1.5.2'
 WEBSITE_VERSION='0.6.12'
-LIBRARY_VERSION='0.10.2'
+LIBRARY_VERSION='0.10.3'
 
 usage() {
   echo "USAGE: $0 -w WORKERS [-s STAMP] [-l]" >&2
