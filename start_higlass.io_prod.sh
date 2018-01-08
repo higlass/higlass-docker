8ba1b7e6bda95d847eabcc6671a987362b801260
Fritz Lekschas <service@lekschas.de>
2018-01-08 17:16:21 +0100

    Fix hga setup for HiGlass.io

--- start_higlass.io_prod.sh
+++ start_higlass.io_prod.sh
@@ -8,7 +8,5 @@ STAMP=`date +"%Y-%m-%d_%H-%M-%S"`
 ./start_production.sh $STAMP
 
 # Set HiGlass.io
-# 1. copy config
-docker exec -it container-$STAMP-with-redis cp higlass-app/config.json higlass-app/config.local.json
-# 2. enable HiGlass.io homepage
-docker exec -it container-$STAMP-with-redis sed -i 's/"homepageDemos": false/"homepageDemos": true/' higlass-app/config.local.json
+# 1. enable HiGlass.io homepage
+docker exec -it container-$STAMP-with-redis sed -i 's/HGAC_HOMEPAGE_DEMOS=false/HGAC_HOMEPAGE_DEMOS=true/' higlass-app/config.js
