1c63113e276f32a7713b48c99574427d9f017605
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-01-21 11:23:44 -0500

    Bumped library version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 STAMP='default'
 SERVER_VERSION='1.5.2'
 WEBSITE_VERSION='0.6.12'
-LIBRARY_VERSION='0.10.12'
+LIBRARY_VERSION='0.10.13'
 
 usage() {
   echo "USAGE: $0 -w WORKERS [-s STAMP] [-l]" >&2
