2a7a953d9a385aded342c739f9e3deac685aecf5
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-02-12 14:39:04 -0500

    Bumped higlass client version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 STAMP='default'
 SERVER_VERSION='1.5.2'
 WEBSITE_VERSION='0.6.17'
-LIBRARY_VERSION='0.10.18'
+LIBRARY_VERSION='0.10.19'
 
 usage() {
   echo "USAGE: $0 -w WORKERS [-s STAMP] [-l]" >&2
