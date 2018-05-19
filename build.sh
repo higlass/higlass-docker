275786a4a3d7cae00dba17bd12a253c9aa97594b
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-05-19 12:45:12 -0400

    Install clodius too

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ WEBSITE_VERSION='0.6.24'
 LIBRARY_VERSION='1.0.0-alpha.2'
 MULTIVEC_VERSION='0.1.5'
 HGTILES_VERSION='0.2.3'
+CLODIUS_VERSION='0.9.1'
 
 usage() {
   echo "USAGE: $0 -w WORKERS [-s STAMP] [-l]" >&2
@@ -35,7 +36,7 @@ fi
 set -o verbose # Keep this after the usage message to reduce clutter.
 
 # When development settles down, consider going back to static Dockerfile.
-perl -pne "s/<HGTILES_VERSION>/$HGTILES_VERSION/g; s/<MULTIVEC_VERSION>/$MULTIVEC_VERSION/g; s/<SERVER_VERSION>/$SERVER_VERSION/g; s/<WEBSITE_VERSION>/$WEBSITE_VERSION/g; s/<LIBRARY_VERSION>/$LIBRARY_VERSION/g" \
+perl -pne "s/<CLODIUS_VERSION>/$CLODIUS_VERSION/g; s/<HGTILES_VERSION>/$HGTILES_VERSION/g; s/<MULTIVEC_VERSION>/$MULTIVEC_VERSION/g; s/<SERVER_VERSION>/$SERVER_VERSION/g; s/<WEBSITE_VERSION>/$WEBSITE_VERSION/g; s/<LIBRARY_VERSION>/$LIBRARY_VERSION/g" \
           web-context/Dockerfile.template > web-context/Dockerfile
 
 REPO=gehlenborglab/higlass
