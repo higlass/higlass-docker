08247afab9114a123a560c538400d9053a5eaff8
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-12-28 23:35:57 -0500

    Fixed test-runner to use stamp in docker-compose

--- test_runner.sh
+++ test_runner.sh
@@ -15,15 +15,15 @@ echo "stamp:", $STAMP
 
 ./build.sh -w 4 -s $STAMP
 
-perl -pne "s/<STAMP>/$STAMP/g" stack-template.yml > stack_test.yml
+perl -pne "s/<STAMP>/$STAMP/g" stack-template.yml > stack-test.yml
 
 test_standalone() {
     # Keep this simple: We want folks just to be able to run the bare Docker container.
     # If this starts to get sufficiently complicated that we want to put it in a script
     # by itself, then it has gotten too complicated.
-    docker-compose -f stack_test.yml down
-    docker-compose -f stack_test.yml rm
-    docker-compose -f stack_test.yml up -d
+    docker-compose -f stack-test.yml down
+    docker-compose -f stack-test.yml rm
+    docker-compose -f stack-test.yml up -d
     python tests.py
 }
 
