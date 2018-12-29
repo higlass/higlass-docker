872f34ae5a62775f64858b8acf6682b7f6fe836b
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-12-29 14:39:15 -0500

    Added shell=True to call

--- tests.py
+++ tests.py
@@ -20,7 +20,7 @@ class CommandlineTest(unittest.TestCase):
             time.sleep(1)
             counter -= 1
         if counter == 0:
-            subprocess.call('docker exec container-{STAMP}{SUFFIX} ./logs.sh')
+            subprocess.call('docker exec container-{STAMP}{SUFFIX} ./logs.sh', shell=True)
 
     def assertRun(self, command, res=[r'']):
         output = subprocess.check_output(command.format(**os.environ), shell=True).strip()
