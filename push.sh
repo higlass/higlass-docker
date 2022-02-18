27304647b7f3dd5e127c187aad87ab33e38b4f27
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2022-02-17 21:12:16 -0800

    Tag image with DOCKER_VERSION

--- push.sh
+++ push.sh
@@ -2,5 +2,6 @@ DOCKER_VERSION='0.10.4'
 REPO=higlass/higlass-docker
 
 sudo docker login -u $DOCKER_USER -p $DOCKER_PASS
+sudo docker tag ${REPO}:latest ${REPO}:${DOCKER_VERSION}
 sudo docker push ${REPO}:${DOCKER_VERSION}
 sudo docker push ${REPO}:latest
